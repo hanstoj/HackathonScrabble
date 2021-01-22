67186a279e60a370ac77ed8308c346723dcdc5c7
hanstoj <stojack.hannah@gmail.com>
2021-01-22 14:47:42 -0800

    notes taken for letter requirements

---
+++ script.js
@@ -0,0 +1,15 @@
+// json ob of letters points and quantity
+
+//
+// A-9, B-2, C-2, D-4, E-12, F-2, G-3, H-2, I-9, J-1, K-1, L-4, M-2, N-6, O-8, P-2, Q-1, R-6, S-4, T-6, U-4, V-2, W-2, X-1, Y-2, Z-1 and Blanks-2.
+
+// (1 point)-A, E, I, O, U, L, N, S, T, R
+// (2 points)-D, G
+// (3 points)-B, C, M, P
+// (4 points)-F, H, V, W, Y
+// (5 points)-K
+// (8 points)- J, X
+// (10 points)-Q, Z
+Letters = {
+
+}
---
+++ server.py
@@ -0,0 +1,1 @@
+# created for potential backend
